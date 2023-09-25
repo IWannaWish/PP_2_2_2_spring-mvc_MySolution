90d2ea320bd31db6d13a011b338e22767079b49d
IWannaWish <138825123+IWannaWish@users.noreply.github.com>
2023-09-25 22:49:06 +0300

    Rename "@Component" to "@Service"

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,12 +1,12 @@
 package web.service;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
-
-@Component
+@Service
 public class CarServiceImpl implements CarService {
 
     private static List<Car> cars = new ArrayList<>();
@@ -14,7 +14,7 @@ public class CarServiceImpl implements CarService {
     static {
         cars.add(new Car(1, "Honda", "Civic Type R"));
         cars.add(new Car(2, "Nissan", "GTR"));
-        cars.add(new Car(3, "Infinity", "Q35"));
+        cars.add(new Car(3, "Infiniti", "Q60"));
         cars.add(new Car(4, "Audi", "A5"));
         cars.add(new Car(5, "Lexus", "NX300"));
     }
